fd4dc1e1a647a29e7502b8a292718d4558724ee0
Guilherme Martins <guis.1177@gmail.com>
2022-06-28 01:10:14 -0300

    Criação do  Header e Logo. Alteração das cores do Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import {gql, useQuery } from "@apollo/client"
+import { Event } from "./page/Event";
 
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lessons  {
-  id: string;
-  title: string;
-}
-
-export function App() {
-  const { data } = useQuery<{ lessons: Lessons[]}>(GET_LESSONS_QUERY)
-
-  console.log(data);
-
-  return (
-    <div>
-      <ul>
-        {data?.lessons.map(lessons => {
-          return <li key={lessons.id}>{lessons.title}</li>
-        })}
-        <li></li>
-      </ul>
-    </div>
+export function App (){
+  return(
+    <>
+      <Event/>      
+    </>
   )
 }
- 
---
+++ src/page/Event.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Lesson } from "../components/Lesson";
+import { Sidebar } from "../components/Sidebar";
+import { Video } from "../components/Video";
+
+export function Event (){
+  return(
+    <div>
+      <Header/>
+      {/* <Lesson/>
+      <Sidebar/>
+      <Video/>
+      <Footer/> */}
+    </div>
+  )
+}
